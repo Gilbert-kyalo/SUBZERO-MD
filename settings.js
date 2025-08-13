9c225410a78a61a0955ab32012858ba9d0593046
GILBERT KYALO <158613250+Gilbert-kyalo@users.noreply.github.com>
2025-08-13 20:53:59 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;3MtjXJgY#xXi6loUKP_Cuy2h3zNgCLv4JeAmQiEiFdXPXAxcgme0", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;2NUEnBhJ#a2TYOoKSKgNNh80NLaJ_GaJpk-MBV1xOmYpW6gmI1bM", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "254795443901", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
