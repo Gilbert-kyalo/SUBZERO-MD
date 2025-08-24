bc734dc82343803f3b2ab75f7c6102e804ae865b
name = "Gilbert Kyalo" print(name) <gilbertkyalo254@gmail.com>
2025-08-24 12:42:07 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;qUNiRCSZ#79DzC6Cmq4S7igYV_CjdiGYJqnd5KrnDccyWY4MIoKk", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "254795443901", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
